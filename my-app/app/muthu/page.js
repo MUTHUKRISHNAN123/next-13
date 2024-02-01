30ea2b7206cfac62a4f2e1c938b60596203b0c8c
MUTHUKRISHNAN M <murugesanmuthukrishnan1652004@gmail.com>
2024-02-01 20:34:03 +0530

    init

---
+++ my-app/app/muthu/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="justfiy text bg-col">
+     <h1>Muthu route</h1>
+    </div>
+  );
+}
---
+++ nextui-app/app/docs/page.tsx
@@ -0,0 +1,13 @@
+import {Input} from "@nextui-org/react";
+import {Chip} from "@nextui-org/react";
+
+export default function DocsPage() {
+	return (
+		<div>
+			<Input />
+			<div className="mb-6">
+				<Chip className="justify-between">Muthu</Chip>
+			</div>
+		</div>
+	);
+}
